a6faa234f12f0bb6d6b68405f0f7f8c54afbb2d2
Jeffrey van Rossum <rossum.jeffrey@gmail.com>
2020-01-21 21:58:28 +0100

    Add reference.

---
+++ resources/views/partials/reference.blade.php
@@ -0,0 +1,42 @@
+<div class="mt-10 bg-white p-10 shadow rounded text-lg italic text-gray-800">
+    @if(app()->getLocale() == 'nl')
+    <blockquote>
+        <div class="absolute pin-t pin-l -mt-4 text-gray-300 -ml-4">
+            <i class="fas fa-quote-left text-3xl"></i>
+        </div>
+        <div class="relative leading-relaxed">
+            <p>Jeffrey is een developer met oog voor detail, maar blijft altijd de grote lijnen zien.</p>
+            <p class="mt-4">Een developer met oog voor vormgeving, met gevoel voor taal, uitstekende
+                sociale vaardigheden maar bovenal een hele prettige collega.</p>
+        </div>
+        <div class="mt-10 flex items-center justify-between">
+            <div>
+                <div class="uppercase text-gray-900 font-semibold mb-2">Jan Egbert Krikken</div>
+                <div class="text-gray-700 text-base">Digital Marketing bij Geomares</div>
+            </div>
+            <div>
+                <a href="https://nl.linkedin.com/in/jan-egbert-krikken-0b954720" class="text-blue-500"><i class="fab fa-linkedin text-3xl"></i></a>
+            </div>
+        </div>
+    </blockquote>
+    @else
+    <blockquote>
+        <div class="absolute pin-t pin-l -mt-4 text-gray-300 -ml-4">
+            <i class="fas fa-quote-left text-3xl"></i>
+        </div>
+        <div class="relative leading-relaxed">
+            <p>Jeffrey is a developer with an eye for detail, but always keeps seeing the big picture.</p>
+            <p class="mt-4">A developer with an eye for design, with a sense of language, excellent social skills but above all a very pleasant colleague.</p>
+        </div>
+    </blockquote>
+    <div class="mt-10 flex items-center justify-between">
+        <div>
+            <div class="uppercase text-gray-900 font-semibold mb-2">Jan Egbert Krikken</div>
+            <div class="text-gray-700 text-base">Digital Marketing at Geomares</div>
+        </div>
+        <div>
+            <a href="https://nl.linkedin.com/in/jan-egbert-krikken-0b954720" class="text-blue-500"><i class="fab fa-linkedin text-3xl"></i></a>
+        </div>
+    </div>
+    @endif
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app', [
-    'title' => __('Freelance PHP developer specialized in Laravel and WordPress')
+'title' => __('Freelance PHP developer specialized in Laravel and WordPress')
 ])
 
 @section('content')
-<div class="bg-no-repeat relative pb-12 px-8 md:px-0">
+<div class="bg-no-repeat relative pb-6 px-8 md:px-0">
     <div class="max-w-xl md:max-w-5xl mx-auto relative">
         <div class="flex flex-wrap py-6 md:py-12">
             <div class="w-full md:w-2/3 md:pr-12">
@@ -13,17 +13,18 @@
                     {{ __('I am a freelance PHP Developer based in The Netherlands, specialized in Laravel and WordPress development.') }}
                 </p>
                 <p class="mb-8">
-                    <a href="/about" class="bg-gray-900 hover:bg-gray-800 text-white font-semibold py-3 px-4 shadow mr-4">
+                    <a href="/about"
+                        class="bg-gray-900 hover:bg-gray-800 text-white font-semibold py-3 px-4 shadow mr-4">
                         {{ __('More about me') }}
                     </a>
-                    <a href="mailto:jeffrey@vanrossum.dev" class="bg-pink-500 hover:bg-pink-600 text-white font-semibold py-3 px-4 shadow">
+                    <a href="mailto:jeffrey@vanrossum.dev"
+                        class="bg-pink-500 hover:bg-pink-600 text-white font-semibold py-3 px-4 shadow">
                         {{ __('Hire me') }}
                     </a>
                 </p>
             </div>
             <div class="w-full md:w-1/3 md:pl-12">
-                <img class="shadow-lg rounded"
-                    src="{{ url('images/avatar.jpg') }}">
+                <img class="shadow-lg relative z-10 rounded" src="{{ url('images/avatar.jpg') }}">
             </div>
         </div>
     </div>
@@ -35,10 +36,19 @@
 
 <div class="bg-white py-12 relative px-8 md:px-0">
     <div class="max-w-xl md:max-w-5xl mx-auto">
-        <h2 class="text-4xl font-bold mb-4 sm:mb-12 text-gray-800">{{ __('Latest posts') }}</h2>
-        @foreach ( $posts as $post )
-            @include('posts.block', ['post' => $post])
-        @endforeach
+        <div class="px-12">
+            <div class="md:flex -mx-24">
+                <div class="md:w-1/2 px-8">
+                    <h2 class="text-4xl font-bold mb-4 sm:mb-12 text-gray-800">{{ __('Latest posts') }}</h2>
+                    @foreach ( $posts as $post )
+                    @include('posts.block', ['post' => $post])
+                    @endforeach
+                </div>
+                <div class="md:w-1/2 px-8">
+                    @include('partials.reference')
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
@@ -47,7 +57,7 @@
         <h2 class="text-4xl text-center sm:text-left font-bold mb-4 sm:mb-12 text-gray-800">{{ __('Projects') }}</h2>
         <div class="flex flex-wrap md:mb-16 -mx-4">
             @foreach($projects as $project)
-                @include('projects.block', ['project' => $project])
+            @include('projects.block', ['project' => $project])
             @endforeach
         </div>
     </div>
