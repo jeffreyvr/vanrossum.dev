c2437929e101b9177bd6428330e03e1e0106488a
Jeffrey van Rossum <rossum.jeffrey@gmail.com>
2020-01-21 17:37:59 +0100

    Add favicons.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,11 @@
 
     @yield('seo')
 
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/images/icons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/images/icons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/images/icons/favicon-16x16.png') }}">
+    <link rel="shortcut icon" href="{{ asset('/images/icons/favicon.ico') }}">
+
     <link rel="webmention" href="https://webmention.io/vanrossum.dev/webmention" />
     <link rel="pingback" href="https://webmention.io/vanrossum.dev/xmlrpc" />
 
