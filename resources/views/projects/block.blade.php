b9f2619ad6853584a9169b8a904da322b194fcb9
Jeffrey van Rossum <jeffrey@vanrossum.dev>
2020-07-23 23:49:29 +0200

    Add hover on projects.

--- resources/views/projects/block.blade.php
+++ resources/views/projects/block.blade.php
@@ -1,7 +1,7 @@
 <div class="md:w-1/3 md:px-6 mb-6 md:mb-0">
     <a href="{{ $project['url'] }}" target="_blank">
         <div
-            class="bg-white enlarge rounded text-center text-base rounded-lg border-t-6 border-primary shadow-lg h-full px-8 md:px-12 py-8">
+            class="bg-white enlarge rounded text-center text-base rounded-lg border-t-6 border-primary shadow-lg hover:shadow-2xl h-full px-8 md:px-12 py-8">
             <div class="h-24 pt-5 mb-6">
                 {!! $project['logo'] !!}
             </div>
