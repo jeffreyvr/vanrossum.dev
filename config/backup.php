33f11c73758ea9e4001179b5eb2074acbb7c8dcc
Jeffrey van Rossum <jeffrey@vanrossum.dev>
2022-10-17 22:56:20 +0200

    wip

--- config/backup.php
+++ config/backup.php
@@ -151,12 +151,13 @@ return [
     'notifications' => [
 
         'notifications' => [
+
             \Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class => ['slack'],
-            \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => ['slack'],
-            \Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class => ['slack'],
+//            \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => ['slack'],
+//            \Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class => ['slack'],
             \Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class => ['slack'],
-            \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class => [],
-            \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class => [],
+//            \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class => ['slack'],
+//            \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class => ['slack'],
         ],
 
         /*
