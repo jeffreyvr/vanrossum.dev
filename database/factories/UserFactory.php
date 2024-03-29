93c997d1b217dd1fa8dc268e9119a712a8b150aa
Shift <shift@laravelshift.com>
2024-03-29 10:39:12 +0000

    Shift core files

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+use Illuminate\Support\Facades\Hash;
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -21,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name(),
         'email' => $faker->unique()->safeEmail(),
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => static::$password ??= Hash::make('password'),
         'remember_token' => Str::random(10),
     ];
 });
